1b1507c1d5048dc05866b0fc271c41319355e510
ArchithGandla <archithgandla1010@gmail.com>
2023-06-11 02:42:19 +0530

    Added quiz pages result pages profile pages and quiz data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,11 @@ import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
+import User from './components/User';
+import QuizHome from './components/QuizHome';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -14,8 +19,13 @@ function App() {
         <Route path = "/" element = {<Home/>}></Route>
         <Route path = "/login" element = {<Login/>}></Route>
         <Route path = "/signup" element = {<SignUp/>}></Route>
-        <Route path = "/faq" element = {<SignUp/>}></Route>
+        <Route path = "/faq" element = {<NotFound/>}></Route>
+        <Route path = "/user" element = {<User/>}></Route>
         <Route path = "*" element = {<NotFound/>}></Route>
+        <Route path = "/QuizHome" element = {<QuizHome/>}></Route>
+        <Route path = "/Quiz" element = {<Quiz/>}></Route>
+        <Route path = "/result" element = {<Result/>}></Route>
+        <Route path = "/profile" element = {<Profile/>}></Route>
       </Routes>
     </>
   );
---
+++ frontend/src/components/Result.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Result = () => {
+
+  const navigate = useNavigate();
+  
+  return (
+    <div className='container'>
+        <div className='row d-flex justify-content-center mt-3'>
+            <div className='col-4 d-flex flex-column gap-3 pt-3'>
+                <h3>Thank you, You are done with the quiz</h3>
+                <h3>See you next week !</h3>
+                <button className='btn btn-primary'
+                onClick={() => navigate("/user")}>GO TO HOME PAGE</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Result
