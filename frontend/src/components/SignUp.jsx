5278c75daebe8d75f96fa86fc7abca2f5ec40c06
ArchithGandla <archithgandla1010@gmail.com>
2023-06-10 21:19:13 +0530

    Added Navbar and signup pages

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -2,7 +2,178 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <section className="vh-100" style={{backgroundColor: "#eee"}}>
+    <div className="container h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-lg-12 col-xl-11">
+          <div className="card text-black" style= {{borderRadius: "25px"}}>
+            <div className="card-body p-md-5">
+              <div className="row justify-content-center">
+                <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                  <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                    Sign up
+                  </p>
+
+                  <form className="mx-1 mx-md-4">
+                    <div className="d-flex flex-row align-items-center mb-4">
+                      <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                      <div className="form-outline flex-fill mb-0">
+                        <input
+                          type="text"
+                          id="form3Example1c"
+                          className="form-control"
+                          name="name"
+                          value=""
+                          onChange={() => {}}
+                          required
+                        />
+                        <label className="form-label" htmlfor="form3Example1c">
+                          Your Name
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                      <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div className="form-outline flex-fill mb-0">
+                        <input
+                          type="text"
+                          id="form3Example3c"
+                          className="form-control"
+                          name="age"
+                          value=""
+                          onChange={() => {}}
+                          required
+                        />
+                        <label className="form-label" htmlfor="age">
+                          Your Age
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                      <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div className="form-outline flex-fill mb-0">
+                        
+                        <input
+                          type="text"
+                          id="form3Example3c"
+                          className="form-control"
+                          name="city"
+                          value=""
+                          onChange={() => {}}
+                          required
+                        />
+                        <label className="form-label" htmlfor="city">
+                          City 
+                        </label>
+
+                        <input
+                          type="text"
+                          id="form3Example3c"
+                          className="form-control mt-4"
+                          name="state"
+                          value=""
+                          onChange={() => {}}
+                          required
+                        />
+                        <label className="form-label" htmlfor="city">
+                          State 
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                      <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div className="form-outline flex-fill mb-0">
+                        <input
+                          type="text"
+                          id="form3Example3c"
+                          className="form-control"
+                          name="age"
+                          value=""
+                          onChange={() => {}}
+                          required
+                        />
+                        <label className="form-label" htmlfor="age">
+                          Guardian Phone Number(Optional*)
+                        </label>
+                      </div>
+                    </div>
+
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                      <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div className="form-outline flex-fill mb-0">
+                        <input
+                          type="password"
+                          id="form3Example4c"
+                          className="form-control"
+                          name="password"
+                          value=""
+                          onChange={() => {}}
+                          required
+                        />
+                        <label className="form-label" for="form3Example4c">
+                          Password
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mb-4">
+                      <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                      <div className="form-outline flex-fill mb-0">
+                        <input
+                          type="password"
+                          id="form3Example4cd"
+                          className="form-control"
+                          required
+                        />
+                        <label className="form-label" for="form3Example4cd">
+                          Repeat your password
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="form-check d-flex justify-content-center mb-5">
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                        value=""
+                        id="form2Example3c"
+                        required
+                      />
+                      <label className="form-check-label" for="form2Example3">
+                        I agree all statements in{" "}
+                        <a href="#!">Terms of service</a>
+                      </label>
+                    </div>
+
+                    <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                      <button
+                        type="button"
+                        className="btn btn-primary btn-lg"
+                        onClick={() => {}}
+                      >
+                        Register
+                      </button> 
+                    </div>
+                  </form>
+                </div>
+                <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                    className="img-fluid"
+                    alt="Sample image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   )
 }
 
