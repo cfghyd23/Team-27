292e3815e2ed5fe9e76770f32f8c560a4956953d
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-06-11 00:10:19 +0530

    feature: added provide help form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
+import ProvideHelp from './ProvideHelp/ProvideHelp';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path = "/" element = {<Home/>}></Route>
-        <Route path = "/login" element = {<Login/>}></Route>
-        <Route path = "/signup" element = {<SignUp/>}></Route>
-        <Route path = "*" element = {<NotFound/>}></Route>
-      </Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/provide-help" element={<ProvideHelp />}></Route >
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes >
     </>
   );
 }
