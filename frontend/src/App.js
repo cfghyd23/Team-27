439b9de2a03b2add4c9de930de6a5b3c7aed2c3c
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2023-06-11 02:21:43 +0530

    added skill development page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
+import SkillDevelopment from './components/SkillDevelopment';
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path = "/" element = {<Home/>}></Route>
+        <Route path = "/skilldevelopment" element = {<SkillDevelopment/>}></Route>
         <Route path = "/login" element = {<Login/>}></Route>
         <Route path = "/signup" element = {<SignUp/>}></Route>
-        <Route path = "/faq" element = {<SignUp/>}></Route>
+        <Route path = "/faq" element = {<faq/>}></Route>
         <Route path = "*" element = {<NotFound/>}></Route>
       </Routes>
     </>
