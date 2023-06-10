82c2e4095d0092720e2ce3b4cc23f940464cca6a
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-06-11 01:53:12 +0530

    feature: implemented admin view of the form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
+import SeekHelpAllForms from './components/SeekHelpAllForms/SeekHelpAllForms';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path = "/" element = {<Home/>}></Route>
-        <Route path = "/login" element = {<Login/>}></Route>
-        <Route path = "/signup" element = {<SignUp/>}></Route>
-        <Route path = "/faq" element = {<SignUp/>}></Route>
-        <Route path = "*" element = {<NotFound/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/faq" element={<SignUp />}></Route>
+        <Route path="/seek-help-all-forms" element={<SeekHelpAllForms />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
