54fa8beadc86ffca5993fbe127d0a0e83e5ae4f0
ArchithGandla <archithgandla1010@gmail.com>
2023-06-11 03:52:16 +0530

    Dealt with CSS conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,26 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import HomePage from './components/HomePage/Home';
+// import HomePage from './components/HomePage/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
 import ProvideHelp from './ProvideHelp/ProvideHelp';
 import FAQ from './components/faq';
-import SeekHelpAllForms from './components/SeekHelpAllForms';
+import SeekHelpAllForms from './components/SeekHelpAllForms/SeekHelpAllForms';
 import SkillDevelopment from './components/SkillDevelopment';
-import faq from './components/faq';
 import User from './components/User';
-import QuizHome from './components/Quiz/QuizHome';
-import Quiz from './components/Quiz/Quiz';
-import Result from './components/Quiz/Result';
-import Profile from './components/Profile/Profile';
+import QuizHome from './components/QuizHome';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/provide-help" element={<ProvideHelp />}></Route >
@@ -30,7 +29,7 @@ function App() {
         <Route path="/skilldevelopment" element={<SkillDevelopment />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/faq" element={<faq />}></Route>
+        <Route path="/faq" element={<FAQ/>}></Route>
         <Route path="/user" element={<User />}></Route>
         <Route path="/QuizHome" element={<QuizHome />}></Route>
         <Route path="/Quiz" element={<Quiz />}></Route>
