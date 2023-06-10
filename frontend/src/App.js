c647969c916849dd1c6327cae0ca6e1de992b187
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-06-11 03:09:32 +0530

    resolve route conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,14 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotFound from './components/NotFound';
 import FAQ from './components/faq';
+import SeekHelpAllForms from './components/SeekHelpAllForms';
+import SkillDevelopment from './components/SkillDevelopment';
+import faq from './components/faq';
+import User from './components/User';
+import QuizHome from './components/Quiz/QuizHome';
+import Quiz from './components/Quiz/Quiz';
+import Result from './components/Quiz/Result';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
@@ -17,16 +25,16 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/seek-help-all-forms" element={<SeekHelpAllForms />}></Route>
-        <Route path = "/" element = {<Home/>}></Route>
-        <Route path = "/skilldevelopment" element = {<SkillDevelopment/>}></Route>
-        <Route path = "/login" element = {<Login/>}></Route>
-        <Route path = "/signup" element = {<SignUp/>}></Route>
-        <Route path = "/faq" element = {<faq/>}></Route>
-        <Route path = "/user" element = {<User/>}></Route>
-        <Route path = "/QuizHome" element = {<QuizHome/>}></Route>
-        <Route path = "/Quiz" element = {<Quiz/>}></Route>
-        <Route path = "/result" element = {<Result/>}></Route>
-        <Route path = "/profile" element = {<Profile/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/skilldevelopment" element={<SkillDevelopment />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/faq" element={<faq />}></Route>
+        <Route path="/user" element={<User />}></Route>
+        <Route path="/QuizHome" element={<QuizHome />}></Route>
+        <Route path="/Quiz" element={<Quiz />}></Route>
+        <Route path="/result" element={<Result />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
