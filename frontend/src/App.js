917ef197273cf8ee254ac0ff62845b4336c8d4e5
ArchithGandla <archithgandla1010@gmail.com>
2023-06-10 20:42:43 +0530

    Added basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import {Route,Routes} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routes>
-        <Route></Route>
+        <Route path = "/" element = {<Home/>}></Route>
+        <Route path = "/login" element = {<Login/>}></Route>
+        <Route path = "/signup" element = {<SignUp/>}></Route>
+        <Route path = "*" element = {<NotFound/>}></Route>
       </Routes>
     </>
   );
---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>Requested page Not Found</div>
+  )
+}
+
+export default NotFound
