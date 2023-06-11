37b9e1cd5cc4fb8b61986be2122477c60f24bd84
ArchithGandla <archithgandla1010@gmail.com>
2023-06-11 08:16:21 +0530

    Customized anonymous route page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>Requested page Not Found</div>
+    <div className='container'>
+      <div className='row'>
+        <div className = 'col-8 rounded d-flex flex-column align-items-center' 
+        style = {{border:"solid black"}}>
+          <h1>The following page doesnot exist</h1>
+          <button className='btn btn-primary'
+          onClick={() => navigate("/")}>GO TO HOME PAGE</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
